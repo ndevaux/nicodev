4cbff44a095dcb442db016d01335091304fbe717
Nicolas Devaux <ndevaux@gmail.com>
2014-10-29 14:42:47 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'github-pages', '~> 28'
+gem 'jekyll-compass', '~> 1.2.0'
+gem 'sass', '~> 3.4.5'
+gem 'sassy-maps', '~> 0.4.0'
+gem 'breakpoint', '~> 2.5.0'
+gem 'susy', '~> 2.1.3'
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber')
+    gutil = require('gulp-util'),
+    notify = require('gulp-notify');
+
+var onError = function (err) {
+    gutil.beep();
+    console.log(err);
+};
+var sassDir = '_sass/';
+var targetCSSDir = 'css/';
+
+
+gulp.task('styles', function() {
+    return gulp.src(sassDir + '**/*.scss')
+        .pipe(plumber({
+            errorHandler: notify.onError("Error: <%= error.message %>")
+        }))
+        //.pipe(notify('SASS compilé !'))
+        .pipe(sass({
+            style: 'compressed',
+            lineNumbers: true,
+            require: ['susy', 'breakpoint']
+        }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+        .pipe(gulp.dest(targetCSSDir));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(sassDir + '**/*.scss', ['styles']);
+});
